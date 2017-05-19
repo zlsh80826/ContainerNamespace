cf180c931e97cbe36155f14ece540454e0425874
zlsh80826 <zlsh80826@gmail.com>
2017-05-19 16:39:09 +0800

    connected with client

--- program/bridge.c
+++ program/bridge.c
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sched.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/wait.h>
+#include <sys/errno.h>
+#include <errno.h>
+
+#define MSGPERM 0600    
+#define MSGTXTLEN 128 
+
+struct msg_buf {
+	long mtype;
+	char mtext[MSGTXTLEN];
+};
 
 void GetContainerPid(int* server_pid, int* client_pid) {
     
@@ -21,6 +35,11 @@ void GetContainerPid(int* server_pid, int* client_pid) {
 }
 
 int main(int argc, char** argv) {
+    int magic = 5566;
+    if (argc > 1) {
+        magic = std::stoi(argv[1]);
+    } 
+
     int server_pid;
     int client_pid;
 
@@ -31,15 +50,40 @@ int main(int argc, char** argv) {
 
     if (setns(open(client_path.c_str(), O_RDONLY), CLONE_NEWIPC)) {
         std::cerr << "Setting ipc same as " + client_path << " met error\n";
+        return 1;
     } else {
         std::cerr << "Setting ipc same as " + client_path << " success\n";
     }
     
     if (setns(open(server_path.c_str(), O_RDONLY), CLONE_NEWNS)) {
         std::cerr << "Setting mnt same as " + server_path << " met error\n";
+        return 1;
     } else {
         std::cerr << "Setting mnt same as " + server_path << " success\n";
     }
 
+    int msgqid, rc;
+    msg_buf msg;
+    msgqid = msgget(magic, MSGPERM | IPC_CREAT | IPC_EXCL);
+
+    if (msgqid < 0) {
+        perror(strerror(errno));
+        fprintf(stderr, "Failed to create message queue with msgqid = %d\n", msgqid);
+        return 1;
+    }
+    
+    rc = msgrcv(msgqid, &msg, sizeof(msg.mtext), 0, 0);
+    if (rc < 0) {
+        perror(strerror(errno));
+        printf("msgrecv failed, rc = %d\n", rc);
+    }
+    printf("Recv: %s\n", msg.mtext);
+
+    rc = msgctl(msgqid, IPC_RMID, NULL);
+    if (rc < 0) {
+        perror(strerror(errno));
+        printf("msgctl (return queue) failed, rc = %d\n", rc);
+        return 1;
+    }
     return 0;
 }
